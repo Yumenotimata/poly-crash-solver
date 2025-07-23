398638122e3bab1aae5391518cb09d6dbba7b860
Yumenotimata <kouyayamaoka@gmail.com>
2025-07-23 10:11:55 +0900

    多分間違ってる凸多面体判定

--- src/convex.rs
+++ src/convex.rs
@@ -8,6 +8,41 @@ impl Convex2 {
     pub fn new(ps: Vec<Vec2>) -> Convex2 {
         Convex2 { ps }
     }
+
+    pub fn is_convex2(ps: Vec<Vec2>) -> bool {
+        let len = ps.len();
+
+        if len <= 1 {
+            return false;
+        }
+
+        let a = ps[0];
+        let b = ps[1];
+        let c = ps[2];
+
+        let p = b - a;
+        let q = c - b;
+
+        let init = p.dot(q) >= 0.0;
+
+        for i in 0..len {
+            let a = ps[i % len];
+            let b = ps[(i + 1) % len];
+            let c = ps[(i + 2) % len];
+
+            let p = b - a;
+            let q = c - b;
+
+            println!("iterate {}, {}, {}", a, b, c);
+
+            if (p.dot(q) >= 0.0) != init {
+                println!("{}, {}, {}", a, b, c);
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 impl Into<Support> for Convex2 {
@@ -26,15 +61,6 @@ impl Support {
     }
 
     pub fn map(&self, v: Vec2) -> Vec2 {
-        // self.ps.iter()
-        //     .fold((0.0, v), |acc: (f32, Vec2), x| {
-        //         let l = x.dot(v);
-        //         if l > acc.0 {
-        //             (l, *x)
-        //         } else {
-        //             acc
-        //         }
-        //     }).1
         *self.ps.iter()
             .max_by(|p, q| p.dot(v).partial_cmp(&q.dot(v)).unwrap())
             .unwrap()
